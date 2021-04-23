9275fdb19ecfc52cdbf1d3151c914b505527350f
Olman <Olmancampos07@hotmail.com>
2021-04-23 14:30:04 +0000

    Final

--- src/js/component/Carousel.js
+++ src/js/component/Carousel.js
@@ -9,7 +9,7 @@ export const Home = () => {
 				<Link to="/characters">
 					<img
 						className="d-block w-100"
-						src="https://images.wallpapersden.com/image/download/star-wars-the-clone-wars-4k_a25pZWaUmZqaraWkpJRobWllrWdma2U.jpg"
+						src="https://www.xtrafondos.com/descargar.php?id=2307&resolucion=1920x1200"
 						width="700"
 						height="830"
 						alt="people"
@@ -34,22 +34,6 @@ export const Home = () => {
 				<Carousel.Caption>
 					<h3>Planetas</h3>
 					<p>Elige tus planetas Favoritos</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-
-			<Carousel.Item>
-				<Link to="/planetsView">
-					<img
-						className="d-block w-100"
-						src="https://www.teahub.io/photos/full/55-555266_star-wars-poster-4k.jpg"
-						width="700"
-						height="830"
-						alt="planets"
-					/>
-				</Link>
-				<Carousel.Caption>
-					<h3>Vehiculos</h3>
-					<p>Elige tus Vehiculos Favoritos</p>
 				</Carousel.Caption>
 			</Carousel.Item>
 		</Carousel>
